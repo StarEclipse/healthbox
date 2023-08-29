41db03ce0e983c251cfb72dae357e01a3881fec6
StarEclipse <kadaj0706t@gmail.com>
2023-08-29 17:53:57 +0800

    add: index.html

---
+++ main.js
@@ -0,0 +1,12 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+
+// plugin -
+import $ from 'jquery';
+window.jQuery = window.$ = $;
+window.AOS = AOS;
+
+
+console.log("main.js 入口檔案");
